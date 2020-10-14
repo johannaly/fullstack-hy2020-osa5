f4a1a09f19f6061ba4da63ab5e80672797e736df
johannaly <johanna@lyytinen.org>
2020-10-14 09:41:25 +0300

    blogilistan frontend 5.2

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -10,99 +10,119 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
- 
+
   useEffect(() => {
     blogService
       .getAll()
       .then(blogs =>
-      setBlogs(blogs)
-    )  
+        setBlogs(blogs)
+      )
   }, [])
 
-/*
-  const addBlog = (event) => 7
-  event.preventDefault()
-  const blogObject = {
-    title: newBlog,
-    id: blogs.length + 1
-  }
-*/
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
 
-const handleLogin = async (event) => {
-  event.preventDefault()
-  console.log('logging in with', username, password)
-try {
-  const user = await loginService.login({
-    username, password
-  })
-  setUser(user)
-  setUsername('')
-  setPassword('')
-} catch (exception) {
-  setErrorMessage("wrong credentials")
-  setTimeout(() => {
-    setErrorMessage(null)
-  }, 5000)
-}
-}
+  /*
+    const addBlog = (event) => 7
+    event.preventDefault()
+    const blogObject = {
+      title: newBlog,
+      id: blogs.length + 1
+    }
+  */
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage("wrong credentials")
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = async (event) => {
+    window.localStorage.clear()
+    setUser(null)
+  }
+
+
+
 
   const loginForm = () => (
-    <form onSubmit = {handleLogin}>
-        <div>
-          username
-          <input 
-              type = "text"
-              value = {username}
-              name = "Username"
-              onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
+    <form onSubmit={handleLogin}>
+      <div>
+        username
           <input
-            type = "password"
-            value = {password}
-            name = "Password"
-            onChange = {({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type = 'submit'>login</button>
-      </form>
-  ) 
-/*
-  const blogForm = () => (
-    <form onSubmit = {addBlog}>
-      <input>
-          value = {newBlog}
-          onChange = {handleBlogChange}
-      </input>
-      <button type = 'submit'>save</button>
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type='submit'>login</button>
     </form>
   )
-*/
+  /*
+    const blogForm = () => (
+      <form onSubmit = {addBlog}>
+        <input>
+            value = {newBlog}
+            onChange = {handleBlogChange}
+        </input>
+        <button type = 'submit'>save</button>
+      </form>
+    )
+  */
 
-  
-if(user === null) {
-  return (
-    <div>
-      <h2>Blogs</h2>
-      <h2>Log in to application</h2>
+
+  if (user === null) {
+    return (
       <div>
-        {loginForm()}
+        <h2>Blogs</h2>
+        <h2>Log in to application</h2>
+        <div>
+          {loginForm()}
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
   return (
     <div>
       <h2>Blogs</h2>
       <p>{user.name} logged in</p>
+      <button onClick={handleLogout} >logout</button>
       <ul>
         {blogs.map(blog =>
-          <Blog 
+          <Blog
             key={blog.id}
-            blog={blog} 
-            />
+            blog={blog}
+          />
         )}
       </ul>
     </div>
