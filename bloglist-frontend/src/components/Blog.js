114219c859bad87832ba819e77828d19c5aa58b2
johannaly <johanna@lyytinen.org>
2020-10-20 14:53:57 +0300

    blogilistan frontend 5.7

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,64 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+
+
+
+
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const [buttonText, setButtonText] = useState('view')
+
+  const blogStyle = {
+    paddingTop:10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const buttonStyle = {
+    marginRight: 5
+  }
+  const toggleVisible = (event) => {
+    if(visible === false) {
+      setButtonText('hide')
+      setVisible(true)
+      console.log('tässä')
+    } else {
+      setButtonText('view')
+      setVisible(false)
+    }
+  }
+
+if(visible === false) {
+  return (
+    <div style = {blogStyle}>
+      <div>
+        <p>
+          {blog.title} {blog.author}
+          <button style = {buttonStyle} onClick= {toggleVisible}>{buttonText}</button>
+        </p> 
+       </div>
+     </div>  
+  )}
+
+
+return (
+    <div style = {blogStyle}>
+      <div>
+        <p>
+          {blog.title} {blog.author}
+          <button style = {buttonStyle} onClick= {toggleVisible}>{buttonText}</button>
+        </p> 
+        <p>{blog.url}</p>
+        <p>
+          {blog.likes}
+          <button style= {buttonStyle}>like</button>
+        </p>
+      </div>
+    </div>
+  )
+}
+
 
 export default Blog
